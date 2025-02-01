c8bbabb2a45c596114e9dcb1fc108fa7689e9a28
vitos-student <aplvbor@gmail.com>
2025-02-01 23:16:45 +0300

    step 2

---
+++ src/Alphabet.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Alphabet {
+
+    private static final List ALPHABET = Arrays.asList('а', 'б','в','г', 'д', 'е', 'ж', 'з', 'и', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у',
+            'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'я', '.', ',', '«', '»', ':', '!', '?', ' ');
+
+    public Alphabet(){
+       // this.ALPHABET
+    }
+
+    public static List<Character> getAlpabet() {
+        return ALPHABET;
+    }
+
+}
--- src/CaesarApp.java
+++ src/CaesarApp.java
@@ -1,6 +1,56 @@
+import java.util.List;
+import java.util.Scanner;
+
 public class CaesarApp {
     public static void main(String[] args) {
-        String str= "First step";
-        System.out.println(str);
+        String str = "пываа";
+        char[] chars = str.toCharArray();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите секретный ключ, от 1 до 300:");
+        int keyCaesar = scanner.nextInt();
+        while (!controlKeyCaesar(keyCaesar)) {
+            System.out.println("Ошибка ввода.");
+            System.out.print("Введите секретный ключ, от 1 до 300:");
+            keyCaesar = scanner.nextInt();
+        };
+        List<Character> listAlphabet = Alphabet.getAlpabet();
+        System.out.println(listAlphabet);
+
+        listAlphabet.size();
+
+        for (int i = 0; i < listAlphabet.size(); i++) {
+            System.out.print(listAlphabet.get(i) + "   ");
+        }
+        System.out.println();
+
+        String cryptString=Сrypt(listAlphabet,str,keyCaesar) ;
+        System.out.println("Было    :" + str);
+        System.out.println("Стало   :" + cryptString);
+
+        String newDecryptString = Сrypt(listAlphabet,cryptString,-keyCaesar);
+        System.out.println();
+        System.out.println();
+        System.out.println("Было    :" + cryptString);
+        System.out.println("Стало   :" + newDecryptString);
+
+    }
+
+    public static String Сrypt(List<Character> listAlphabet,String str,int keyCaesar) {
+        char[] newChar = new char[str.length()];
+        int i =0;
+        for(char ch:str.toCharArray()){
+            newChar[i]=listAlphabet.get(listAlphabet.indexOf(ch) + keyCaesar);
+            i++;
+        }
+        return new String(newChar);
+    }
+
+    private static boolean controlKeyCaesar(int keyCaesar) {
+
+        if (keyCaesar > 0 && keyCaesar <= 300) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
